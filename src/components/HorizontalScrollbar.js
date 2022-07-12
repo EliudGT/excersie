57e8efcfe92d526483659a1fc2cbd571236f1a64
Eliud <oeliud8@gmail.com>
2022-07-12 01:25:53 -0500

    Completing the 30% of the app

---
+++ src/components/HorizontalScrollbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HorizontalScrollbar = () => {
+  return (
+    <div>HorizontalScrollbar</div>
+    /*Me quedo en el minuto 1:00:05 https://www.youtube.com/watch?v=KBpoBc98BwM*/
+  )
+}
+
+export default HorizontalScrollbar
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
+import { Box } from "@mui/system";
+
+import Excercises from "../components/Excercises";
+import SearchExcercises from "../components/SearchExcercises";
+import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
     return ( 
-        <div>
-            
-        </div>
+        <Box>
+            <HeroBanner />
+            <SearchExcercises />
+            <Excercises/>
+        </Box>
      );
 }
  
